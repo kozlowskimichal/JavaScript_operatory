0b2b3386622508561c08ea80a82bcba3ab9c98a5
Michał Kozłowski <kozlowskimichal88@gmail.com>
2017-06-10 14:35:27 +0200

    podstawowe uzycie operatorow

---
+++ js/operators.js
@@ -0,0 +1,29 @@
+'use strict';
+
+var liczba1 =13, liczba2=5;
+
+var wynik;
+
+wynik= liczba1 % liczba2;
+console.log(wynik);
+
+var licz =2 ;
+licz += 3;
+// to jest skrócony zapis tego co wyzej licz = licz +3;
+
+// sprawdzanie przedzialu
+
+if ( (liczba2 > 3) && (liczba2 < 10) ) {
+    console.log("tak");
+} 
+
+
+//zapis ponizej jest taki sam . ? znaczy if, a : znaczy else 
+
+(2>5) ? console.log("tak") : console.log("nie");
+
+if (2>5) {
+    console.log("tak");
+}  else {
+    console.log("nie");
+}
